bb5d6538bef7c43d6505079b0a38bb8c9470aa03
Eduardo <eduardof.dutra@yahoo.com>
2021-02-25 15:03:52 -0300

    primeiro commit

---
+++ src/pong/Enemy.java
@@ -0,0 +1,24 @@
+package pong;
+
+import java.awt.Graphics;
+
+public class Enemy {
+	
+	public double x,y,width,height;
+	
+	public Enemy(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 10;
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void render(Graphics g) {
+		
+	}
+	
+}
